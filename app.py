c491270b691a6f1f8bd1c37ae7485103a1c65148
Waqas Arif <wakas.arif@hotmail.com>
2023-12-06 17:02:15 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-# app.py
 import streamlit as st
+import joblib
+import numpy as np
 
-def main():
-    # Title of the web app
-    st.title('Form with 10 Input Fields')
+# Load the trained model
+model = joblib.load('your_model.joblib')
 
-    # Create a form with 10 input fields
-    with st.form('Input Form'):
-        # Create 10 input fields
-        input_fields = []
-        for i in range(10):
-            input_fields.append(st.text_input(f'Input {i+1}', key=f'input_{i+1}', value=''))
+# Title of the web app
+st.title('Machine Learning Model Deployment')
 
-        # Add a submit button to the form
-        submitted = st.form_submit_button('Submit')
+# Create input fields for the form
+input_fields = []
+for i in range(10):
+    input_fields.append(st.text_input(f'Input {i+1}', value=''))
 
-        # When the form is submitted
-        if submitted:
-            # Display the input data
-            for i, value in enumerate(input_fields):
-                st.write(f'Input {i+1}: {value}')
+# Convert input data to a numpy array
+input_data = np.array([float(field) for field in input_fields])
 
-if __name__ == '__main__':
-    main()
+# Make a prediction when a button is clicked
+if st.button('Predict'):
+    # Reshape the input data to match the model's expected input shape
+    input_data = input_data.reshape(1, -1)
+    
+    # Make prediction
+    prediction = model.predict(input_data)
+    
+    # Display the prediction
+    st.write(f'The predicted output is: {prediction[0]}')
